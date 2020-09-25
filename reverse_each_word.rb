abd3e407099b78528517e3305a86ec0bd92ec348
Anshul Arora <a.99.anshul@gmail.com>
2020-09-25 20:28:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(sentence)
   split_sentence = sentence.split
   split_sentence.collect do |word|
-    binding,pry  
+    binding.pry  
   end
   
   result = split_sentence.join(" ")

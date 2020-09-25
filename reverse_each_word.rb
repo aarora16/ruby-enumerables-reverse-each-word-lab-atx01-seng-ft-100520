13cc26a4c41a8c7afcbf8acb1001b0ba1a9a0c9c
Anshul Arora <a.99.anshul@gmail.com>
2020-09-25 20:21:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 require 'pry'
 def reverse_each_word(sentence)
-  sentence.each do |letter|
-    letter
-    binding.pry
-  end
+  split_sentence = sentence.split
+  # sentence.each do |letter|
+  #   letter
+  #   binding.pry
+  # end
 end
 
 reverse_each_word("Hello there, and how are you?")

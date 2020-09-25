c61e9885e7882d8704bb3c59c71cff390a37742b
Anshul Arora <a.99.anshul@gmail.com>
2020-09-25 20:27:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,7 @@
 require 'pry'
 def reverse_each_word(sentence)
   split_sentence = sentence.split
-  split_sentence.each do |letter|
-    letter.reverse!
-  end
-  result = split_sentence.join(" ")
+  split_sentence.collect {|word| word.reverse}
 end
 
 reverse_each_word("Hello there, and how are you?")
